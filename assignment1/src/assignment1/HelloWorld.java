2ea8995ba5496f93a07f45d6a22da03599599c8f
Thomas Gassmann <thomas.gassmann@hotmail.com>
2022-02-23 00:16:00 +0100

    Add assignment 1
    Signed-off-by: Thomas Gassmann <thomas.gassmann@hotmail.com>

---
+++ assignment1/src/assignment1/HelloWorld.java
@@ -0,0 +1,12 @@
+package assignment1;
+/* This is a class */
+public class HelloWorld {
+	/*
+	* This is the main method.
+	* This is the entry point for your program.
+	*/
+	public static void main(String[] arguments) {
+	/* Output Hello World to the console */
+		System.out.println("Hello World");
+	}
+}
