0a2031fb2374a38d4f450fdc80f3f9d8307cb59f
Thomas Gassmann <thomas.gassmann@hotmail.com>
2022-03-23 20:51:01 +0100

    Task A
    Signed-off-by: Thomas Gassmann <thomas.gassmann@hotmail.com>

--- assignment5/src/ethz/ch/pp/searchAndCount/SearchAndCountSeqDivideAndConquer.java
+++ assignment5/src/ethz/ch/pp/searchAndCount/SearchAndCountSeqDivideAndConquer.java
@@ -3,10 +3,28 @@ package ethz.ch.pp.searchAndCount;
 import ethz.ch.pp.util.Workload;
 
 public class SearchAndCountSeqDivideAndConquer {
-
+	
 	public static int countNoAppearances(int[] input, Workload.Type wt) {
-		//TODO implement
-		return 0;
+		return count(input, wt, 0, input.length - 1);
+	}
+	
+	public static int count(int[] input, Workload.Type type, int from, int to) {
+		if (to - from <= 2) {
+			int c = 0;
+			for (int i = from; i <= to; i++) {
+				if (Workload.doWork(input[i], type)) {
+					c++;
+				}
+			}
+			
+			return c;
+		}
+
+		int s = (to - from) / 2;
+		int l = count(input, type, from, from + s);
+		int r = count(input, type, from + s + 1, to);
+		
+		return l + r;
 	}
 
 }
