3946aaba46c0928b5474503ef3a152e8a6043f8c
Thomas Gassmann <thomas.gassmann@hotmail.com>
2022-04-15 17:40:57 +0200

    Update atomic random
    Signed-off-by: Thomas Gassmann <thomas.gassmann@hotmail.com>

--- assignment8/src/assignment8/random/AtomicRandom.java
+++ assignment8/src/assignment8/random/AtomicRandom.java
@@ -5,20 +5,27 @@ import java.util.concurrent.atomic.AtomicLong;
 public class AtomicRandom implements RandomInterface {
     private static final long a = 25214903917L;
     private static final long c = 11;
-    private long state;
-
+    private AtomicLong state;
+    
     public AtomicRandom(long seed) {
-        state = seed;
+        state = new AtomicLong(seed);
     }
 
     @Override
     public int nextInt() {
     	// get the current seed value
-    	long orig = state;
-    	// using recurrence equation to generate next seed
-    	long next = (a * orig + c) & (~0L >>> 16);
-    	// store the updated seed
-    	 state = next;
-         return (int) (next >>> 16);
+    	long next = 0;
+    	while (true) {
+        	long orig = state.get();
+        	// using recurrence equation to generate next seed
+        	next = (a * orig + c) & (~0L >>> 16);
+        	
+        	if (state.compareAndSet(orig, next)) {
+        		break;
+        	}
+    	}
+    	
+    	
+    	return (int) (next >>> 16);
     }
 }
