aa07a94e6eb1129f37fa916eeb8b46a939ce63e1
Thomas Gassmann <thomas.gassmann@hotmail.com>
2022-06-07 17:28:20 +0200

    Exercise 1
    Signed-off-by: Thomas Gassmann <thomas.gassmann@hotmail.com>

--- assignment14/src/assignment14/stm/CircularBufferSTM.java
+++ assignment14/src/assignment14/stm/CircularBufferSTM.java
@@ -11,26 +11,71 @@ import java.util.concurrent.Callable;
  * specifically using ScalaSTM [http://nbronson.github.io/scala-stm/]).
  */
 public class CircularBufferSTM<E> implements CircularBuffer<E> {
-    CircularBufferSTM(int capacity) {
+	private Ref.View<Integer> _head = STM.newRef(0);
+	private Ref.View<Integer> _tail = STM.newRef(0);
+	private Ref.View<Integer> _count = STM.newRef(0);
+	private TArray.View<E> _items = null;
+	
+	CircularBufferSTM(int capacity) {
         // TODO: implement: create TArray of size capacity.
+		_items = STM.newTArray(capacity);
     }
 
     public void put(final E item) {
         // TODO: implement: add item to the queue, otherwise block until a slot is free.
+    	STM.atomic(new Runnable() {
+    		@Override
+    		public void run() {
+    			if (isFull()) {
+    				STM.retry();
+    			}
+    			
+    			_items.update(_head.get(), item);
+    			_head.set(inc(_head.get()));
+    			STM.increment(_count, 1);
+    		}
+    	});
     }
 
     public E take() {
         // TODO: implement: return the oldest item or block until an item is present.
-        return null;
+    	return STM.atomic(new Callable<E>() {
+    		@Override
+    		public E call() {
+    			if (isEmpty()) {
+    				STM.retry();
+    			}
+    			
+    			var item = _items.refViews().apply(_tail.get()).get();
+    			_items.update(_tail.get(), null);
+    			_tail.set(inc(_tail.get()));
+    			STM.increment(_count, -1);
+    			return item;
+    		}
+    	});
     }
 
     public boolean isEmpty() {
         // TODO: implement: helper method which can be used above.
-        return false;
+    	return STM.atomic(new Callable<Boolean>() {
+    		@Override
+    		public Boolean call() {
+    			return _count.get() == 0;
+    		}
+    	});
     }
 
     public boolean isFull() {
         // TODO: implement: helper method which can be used above.
-        return false;
+    	return STM.atomic(new Callable<Boolean>() {
+    		@Override
+    		public Boolean call() {
+    			return _count.get() == _items.length();
+    		}
+    	});
+    }
+    
+    private int inc(int i) {
+    	return (i + 1) % _items.length();
     }
 }
